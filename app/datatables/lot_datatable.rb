300d795c214a4a7edc4f46aa70732bd6719b650e
Hien Nguyen <quochien@gmail.com>
2019-01-21 09:27:15 +0700

    update columns

--- app/datatables/lot_datatable.rb
+++ app/datatables/lot_datatable.rb
@@ -65,7 +65,7 @@ class LotDatatable < BaseDatatable
         price_without_vat_exclude_furniture: lot.price_without_vat_exclude_furniture,
         loyer_ht: lot.loyer_ht,
         price_of_furniture_exclude_vat: lot.price_of_furniture_exclude_vat,
-        # total_price_exclude_vat: lot.total_price_exclude_vat,
+        total_price_exclude_vat: lot.total_price_exclude_vat&.html_safe,
         # pinel_rental: lot.pinel_rental,
         # pinel_rentabilite: lot.pinel_rentabilite,
         images: display_images(lot),

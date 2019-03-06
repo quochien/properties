a6c178410f7004e0b4d1535e79e528e1aaab22a3
Hien Nguyen <quochien@gmail.com>
2019-03-06 23:02:31 +0700

    nexity: login

--- app/services/scrapers/nexity.rb
+++ app/services/scrapers/nexity.rb
@@ -9,5 +9,27 @@ class Scrapers::Nexity < Scrapers::BaseScraper
 
   def perform
     Lot.where(site_id: site.id).update_all(enabled: false)
+
+    puts "start scraping #{site.url}"
+    session = Mechanize.new
+
+    puts "login"
+    login(session)
+
+    page = session.get('https://monbookimmo.fr/Recherche/RechercheLot/Recherche')
+    puts page.title
+  end
+
+  private
+
+  def login(session)
+    page = session.get('https://monbookimmo.fr')
+    puts page.title
+
+    page = session.post(
+      'https://monbookimmo.fr/Authentification/Connexion/AuthentificationUtilisateur',
+      'model.Login' => '998461',
+      'model.Password' => 'Jenexity1@'
+    )
   end
 end
